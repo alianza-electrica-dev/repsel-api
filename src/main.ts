b40ada0b4d27698eab59bcb17c2654e22f44309d
oscar lopez <o.lrodriguez1195@gmail.com>
2024-11-27 17:00:53 -0600

    feat(main):show_ip

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { exec } from 'child_process';
 
 async function main() {
+  exec('curl ifconfig.me', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error al obtener la IP: ${stderr}`);
+    } else {
+      console.log(`IP pública del servidor: ${stdout.trim()}`);
+    }
+  });
+
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
 
