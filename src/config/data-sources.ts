8190192a1df4d860981a91d5343fdeb4066276bf
oscar lopez <o.lrodriguez1195@gmail.com>
2024-11-27 16:44:35 -0600

    feat(data-sources):add_request_timeout

--- src/config/data-sources.ts
+++ src/config/data-sources.ts
@@ -14,6 +14,7 @@ export const createDataSources = (configService: ConfigService) => ({
       encrypt: true,
       trustServerCertificate: true,
     },
+    requestTimeout: 3000,
   }),
 
   dbFG: new DataSource({
@@ -28,5 +29,6 @@ export const createDataSources = (configService: ConfigService) => ({
       encrypt: true,
       trustServerCertificate: true,
     },
+    requestTimeout: 3000,
   }),
 });
