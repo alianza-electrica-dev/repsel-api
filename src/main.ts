77f7a8df810661cf65d468fa5d827f95c9cdfcdf
oscar lopez <o.lrodriguez1195@gmail.com>
2024-11-27 16:34:33 -0600

    feat(main):add_env_port

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 
+const port = +process.env.APP_PORT;
+
 async function main() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
-  await app.listen(3000);
-  Logger.log(`Repsel API running on port 3000`);
+  await app.listen(port);
+  Logger.log(`Repsel API running on port ${port}`);
 }
 main();
